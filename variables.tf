6017cfbabb90473c85caf9272213816af18c6cd4
VishwaTechLabs <iam.infosecvishwa@gmail.com>
2026-06-30 20:54:10 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR Block for VPC"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "VPC Name"
+  type        = string
+}
